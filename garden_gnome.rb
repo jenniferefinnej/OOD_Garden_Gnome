fbe6c143ea5217b3de6c3fcb915e9a13fe07affb
Jennifer Nguyen <j3nniferefinn3j@gmail.com>
2016-02-29 14:25:50 -0600

    building gnome

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,57 @@
 # Code your instances here
+class GardenGnome
+	def name=(name)
+			@name = name 
+	end
+
+	def name 
+			return @name 
+	end 
+
+	def age=(age)
+			@age = age
+	end
+
+	def age 
+			return @age 
+	end
+
+
+	def gluten_allergy(gluten_allergy)
+			@gluten_allergy = gluten_allergy
+	end
+
+	def gluten_allergy
+			return @gluten_allergy
+	end
+
+	def initialize(personality = "evil", hat_color = red)
+			@personality = personality
+			@hat_color = hat_color
+	end
+
+
+	def personality
+			return @personality
+	end
+
+
+	def gnaw
+			return "Gnawing on a tree!!!"
+	end
+
+	def shout
+			return "GNARLY!!!"
+	end
+
+
+	def introduce_self
+			return "Hello humans, my name is #{@name}, I am #{@age}, and you'll rue the day you crossed me!"
+	end
+
+	
+
+
+end
+
+
